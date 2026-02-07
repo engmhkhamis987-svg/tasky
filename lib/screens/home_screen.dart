b80ed9d1e9eda893bf67c7190ad21a82736102cd
Mohammed Hassan <eng.mh.khamis987@gmail.com>
2026-02-07 02:25:01 +0200

    all

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -18,6 +19,9 @@ class _HomeScreenState extends State<HomeScreen> {
   String? userName;
   List<TaskModel> tasks = [];
   bool isLoading = false;
+  int totalTasks = 0;
+  int doneTasks = 0;
+  double percent = 0;
 
   Future<void> _loadUserName() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -34,16 +38,25 @@ class _HomeScreenState extends State<HomeScreen> {
     final finalTasks = prefs.getString('tasks');
     if (finalTasks != null) {
       final List<dynamic> tasksAfterDecode = jsonDecode(finalTasks);
+
       setState(() {
         tasks = tasksAfterDecode
             .map((task) => TaskModel.fromMap(task))
             .toList();
+
+        _calculateProgress();
       });
     }
 
     setState(() {
       isLoading = false;
     });
+  }
+
+  void _calculateProgress() {
+    totalTasks = tasks.length;
+    doneTasks = tasks.where((e) => e.isDone).length;
+    percent = totalTasks == 0 ? 0 : (doneTasks / totalTasks);
   }
 
   @override
@@ -118,6 +131,71 @@ class _HomeScreenState extends State<HomeScreen> {
                   SvgPicture.asset('assets/images/waving_hand.svg'),
                 ],
               ),
+              SizedBox(height: 16),
+              Container(
+                padding: EdgeInsets.all(16),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Color(0XFF282828),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Achieved Tasks',
+                          style: TextStyle(
+                            color: Color(0XFFFFFCFC),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        SizedBox(height: 4),
+                        Text(
+                          '$doneTasks Out of $totalTasks Done',
+                          style: TextStyle(
+                            color: Color(0XFFC6C6C6),
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Transform.rotate(
+                          angle: -pi / 2,
+                          child: SizedBox(
+                            width: 52,
+                            height: 52,
+                            child: CircularProgressIndicator(
+                              value: percent,
+                              // color: Color(0XFF15B86C),
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                Color(0XFF15B86C),
+                              ),
+                              backgroundColor: Color(0XFF6D6D6D),
+                              strokeWidth: 4,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '${(percent * 100).toInt()} %',
+                          style: TextStyle(
+                            color: Color(0XFFFFFCFC),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
 
               SizedBox(height: 20),
               Text(
@@ -138,6 +216,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         onTap: (val, index) async {
                           setState(() {
                             tasks[index!].isDone = val ?? false;
+                            _calculateProgress();
                           });
                           SharedPreferences prefs =
                               await SharedPreferences.getInstance();
